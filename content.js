25aba1a269df9a727f9dc9f5c9aa5412671afdbf
Sana <36829952+cognitive137@users.noreply.github.com>
2020-10-01 01:49:11 +0530

    Update content.js
    Updated lookup table.

--- content.js
+++ content.js
@@ -26,7 +26,9 @@ const lookupTable = {
   "www.theguardian.com":
     'title = document.getElementsByTagName("h1")[0].innerText; pub = "The Guardian"; author = [...document.getElementsByTagName("a")].filter(el => el.rel == "author")[0].innerText; const [z, d, m, y] = [...document.getElementsByTagName("label")].filter(el => el.htmlFor == "dateToggle")[0].innerText.split(" "); DATE = new Date(`${d} ${months.filter(el => el.toLowerCase().startsWith(m.toLowerCase()))[0]} ${y}`);',
   "apnews.com":
-    'const [z, ...rest] = [...document.getElementsByTagName("span")].filter(el => el.innerText.startsWith("By"))[0].innerText.split(" "); name = rest.join(" ").toLowerCase();  [...name].forEach((char, idx) => {if(idx == 0 || name[idx - 1] == " ") author+=char.toUpperCase(); else author+=char});  title=document.getElementsByTagName("h1")[0].innerText; pub="The Associated Press"; DATE= new Date(document.getElementsByClassName("Timestamp")[0].dataset.source)'
+    'const [z, ...rest] = [...document.getElementsByTagName("span")].filter(el => el.innerText.startsWith("By"))[0].innerText.split(" "); name = rest.join(" ").toLowerCase();  [...name].forEach((char, idx) => {if(idx == 0 || name[idx - 1] == " ") author+=char.toUpperCase(); else author+=char});  title=document.getElementsByTagName("h1")[0].innerText; pub="The Associated Press"; DATE= new Date(document.getElementsByClassName("Timestamp")[0].dataset.source)',
+  "www.dw.com":
+    'title = document.getElementsByTagName("h1")[0].innerText; const strongElements = [...document.getElementsByTagName("strong")]; const [d,m,y] = strongElements.filter(el => el.innerText == "Date")[0].parentElement.innerText.replace("Date", "").split("."); DATE = new Date(`${y}/${m}/${d}`); author = strongElements.filter(el => el.innerText == "Author")[0].parentElement.innerText.replace("Author ", ""); pub = "Deutsche Welle"'
 };
 
 function main() {
